fd7a8f7e68c3f1422417face11767d071aa2096b
SevaVigg <vsevolod.makeev@vigg.ru>
2017-08-15 14:37:37 +0300

    Add files via upload

--- assertFileNameCellName.r
+++ assertFileNameCellName.r
@@ -1,23 +1,40 @@
-assertFileNameCellName <- function( cell_lists, FileProperties){
+assertFileNameCellName <- function( cell_list, FileProperties){
 
-assert 			<- list()
-assert$num		<- TRUE
-assert$SampleStr	<- TRUE
-assert$dateEx		<- TRUE
+assertVar <- function(Var) {	ans_attr 	<- list(1) 
+				names(ans_attr) <- "type"
+				attributes(assertVar) <- ans_attr
 
-assertVar <- function(Var, assert) {
-			lapply( cell_lists, function(x) { 
-					if(FileProperties[[Var]] != x[[Var]]) {
-						cat( "Error. Cell ", x[[Var]],  " has ", Var," ", x[[Var]], 
-						     " which does not correspond to file ", Var, " ", FileProperties[[Var]], "\n") 
-						return( assert[[Var]] <- FALSE ) 
-									}
+				if( is.na( FileProperties[[Var]]) & !is.na( cell_list[[Var]])){ 
+						if( Var != "num"){cat("In FileName ", Var, "is missing, updated from CellName ", "\n")}
+						assertVar <- NA; attr(assertVar, "type") <-"take_Cell"
+						return(assertVar) }else{
+				if( is.na( cell_list[[Var]]) & !is.na( FileProperties[[Var]])){
+						cat("In CellName ", Var, "is missing, updated from FileName", "\n" ) 
+						assertVar <- NA; attr(assertVar, "type") <- "take_File"
+						return(assertVar) }else{
+				if( is.na( cell_list[[Var]]) & is.na( FileProperties[[Var]])){
+						if( Var != "dateEx"){
+						   cat( Var, " is missing both in CellName and FileName", "\n" )
+						   assertVar <- FALSE; attr( assertVar, "type") <- paste("Missing CellName and FileName ", Var," values") 
+						   return(assertVar)
+						}else{
+						   cat( Var, "is missing both in CellName and FileName", "\n")
+						   assertVar <- NA; attr(assertVar, "type") <-"take_zero"
+						   return(assertVar) 	
+						} 
+				}else{
+				if(FileProperties[[Var]] != cell_list[[Var]]) {
+						cat( "Warning. CellName ", cell_list[[Var]],  " has ", Var," ", cell_list[[Var]], 
+						     " which does not correspond to FileName's ", Var, " ", FileProperties[[Var]], " ")
+						if( Var == "CellType"){ assertVar <- NA; attr( assertVar, "type") <- "take_File"; cat( " File type is used\n")}else{
+							assertVar <- NA; attr( assertVar, "type") <- "take_Cell"; cat(" Cell type is used\n")}
+						return(assertVar) 
+									}else{ return(TRUE)}}}}
 							} 
-				)
-			}
 
-assertRes	<-sapply( names(assert), function(x) assertVar(x, assert))
+assertRes	<- lapply( names(FileProperties), function(x) assertVar(x))
+names(assertRes)<- names(FileProperties)
 
-assert
+assertRes
 }
 
