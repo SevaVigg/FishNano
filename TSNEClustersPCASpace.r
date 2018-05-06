499e0b09e6a062dd023f36bbc73bf4996869f6bc
SevaVigg <vsevolod.makeev@vigg.ru>
2018-05-06 20:28:39 +0300

    Add files via upload

--- TSNEClustersPCASpace.r
+++ TSNEClustersPCASpace.r
@@ -3,6 +3,9 @@
 #it uses ipmc Seurat object prepared by CalcTSNE_PCASpace.r, which also supplies the number of the PCA
 #components, comps
 
+#dir.create(file.path(getwd(), "Res"), showWarnings = FALSE)
+#dir.create(file.path(getwd(), "Plot"), showWarnings = FALSE)
+
 dir.create(file.path(getwd(), "Res", "PCASpaceClusters"), showWarnings = FALSE)
 dir.create(file.path(getwd(), "Plot", "PCAClusterPlot"), showWarnings = FALSE)
 
@@ -11,21 +14,18 @@ plotDir	<- file.path(getwd(), "Plot", "PCAClusterPlot")
 
 #Fetch Clusters
 
-#resolDec	<- 75			#resolution multiplied by 10, needed to mark files, if commented must be assigned
-#in the calling file
-
+resolDec	<- 6			#resolution multiplied by 10, needed to mark files
 ipmc	 	<- FindClusters(ipmc, reduction.type = "pca", dims.use = 1:comps, resolution = resolDec/10, print.output = 0)
 
 png(file.path(plotDir, paste0("SNN_ClusterTreePCAS", "_c", comps, "_r", resolDec, ".png")))
 	ipmc 	<- BuildClusterTree(ipmc, pcs.use = 1:comps, do.reorder = TRUE, reorder.numeric = TRUE, show.progress = FALSE)
 dev.off()
 
-source("R/assignColors.r")
-
-clusterColors <- assignColors(ipmc)
+Cells <- rbind(Cells, ipmc@ident)
+rownames(Cells)[length(rownames(Cells))]	<- paste0("ClusterPCASp", "_c", comps, "_r", resolDec)
 
 png(file.path(plotDir, paste0("TSNE_ClustersPCAS", "_c", comps, "_r", resolDec, ".png")))
-	TSNEPlot(ipmc, colors.use = clusterColors)
+	TSNEPlot(ipmc)
 dev.off()
 
 #write.table(Cells, file = file.path( resDir, "CellClustersAll.tsv"), sep = "\t")
